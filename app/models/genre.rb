dc0e7ad62700c73fc38594bf02a5dc3641ebcfe9
Janay Bailey <tjanay87@gmail.com>
2019-12-17 19:42:07 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -8,7 +8,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    sekf.artists.count
+    self.artists.count
   end
 
   def all_artist_names
